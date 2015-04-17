7aa183d0e96f02d961d7728e88896d754d4ecd30
markbradford <markbradfordbc@gmail.com>
2015-04-16 23:56:06 -0700

    setting up auth pages, validations

--- models/user.js
+++ models/user.js
@@ -2,9 +2,26 @@
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
     email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,200],
+          msg: 'Password must be at least 8 characters long'
+        }
+      }
+    },
     name: DataTypes.STRING
   }, {
+    hooks: {
+      beforeCreate: function(user,options,sendback){
+        bcrypt.hash(user.password,10,function(err,hash){
+          if(err){ throw err; }
+          user.password=hash;
+          sendback(null,user);
+        });
+      }
+    },
     classMethods: {
       associate: function(models) {
         // associations can be defined here
